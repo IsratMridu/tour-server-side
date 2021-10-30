c2edc301a00db84d870fd93a1a44426ab54598aa
shams-jubair <2017-1-60-032@std.ewubd.edu>
2021-10-30 17:58:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server Ready To Use');
+  })
+  
+  app.listen(port, () => {
+    console.log('Server Running At Port',port);
+  })
+
